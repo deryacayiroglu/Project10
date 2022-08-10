a63bc77cf993e0a31d684b09d8800932fe1db094
Derya Cayiroglu <deryacayroglu@gmail.com>
2022-08-10 20:05:38 +0300

    Initial Commit

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Derya Çayıroğlu on 8.08.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
